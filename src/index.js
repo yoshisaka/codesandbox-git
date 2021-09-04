510baac527acb5a38e0e16ebbc0beeca4d92b0e6
smart <yoshisaka@users.noreply.github.com>
2021-09-04 09:20:00 +0900

    修正１ ああああ あああ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Yoshisaka Hello Vanilla!</h1>
+<h1>Yoshisaka Hello Vanilla!
+aaaaaabbbb
+xxcccc
+
+</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
